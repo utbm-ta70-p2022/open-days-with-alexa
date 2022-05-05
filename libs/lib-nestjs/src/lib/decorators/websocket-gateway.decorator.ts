62d403cd1b266f67ffdd5501489a4366ea53d775
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-05-05 19:22:36 +0200

    remote explicit websocket port configuration

--- libs/lib-nestjs/src/lib/decorators/websocket-gateway.decorator.ts
+++ libs/lib-nestjs/src/lib/decorators/websocket-gateway.decorator.ts
@@ -1,7 +1,7 @@
 import { GatewayMetadata, WebSocketGateway } from '@nestjs/websockets';
 
 export const WebsocketGateway = (): ClassDecorator => {
-  return WebSocketGateway(Number(process.env.WEBSERVICE_PORT), {
+  return WebSocketGateway({
     cors: {
       origin: process.env.WEBSERVICE_ALLOWED_ORIGIN,
     },
