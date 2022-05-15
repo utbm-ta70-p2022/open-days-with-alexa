b1e79baf5573cf40a9b4d37020d859e47307be0a
Malo <malolemestre29@gmail.com>
2022-05-15 18:32:24 +0200

    quickly improving handlers code

--- apps/app-webservice/src/app/domain/alexa-handlers/apprentissage-info.alexa-handler.ts
+++ apps/app-webservice/src/app/domain/alexa-handlers/apprentissage-info.alexa-handler.ts
@@ -1,14 +1,19 @@
 import { HandlerInput, RequestHandler } from 'ask-sdk-core';
 import { Response } from 'ask-sdk-model';
 
+const text = {
+  intentName: "ApprentissageIntent", // french ?
+  speechText: "Voici les informations à propos de l'apprentissage",
+}
+
 export class ApprentissageInfoAlexaHandler implements RequestHandler {
   canHandle(handlerInput: HandlerInput): boolean {
     const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' && request.intent.name === 'AskWeatherIntent';
+    return request.type === 'IntentRequest' && request.intent.name === text.intentName;
   }
 
   handle(handlerInput: HandlerInput): Response {
-    const speechText = "Voici les informations à propos de l'apprentissage";
+    const speechText = text.speechText;
 
     return handlerInput.responseBuilder
       .speak(speechText)
--- apps/app-webservice/src/app/domain/alexa-handlers/fisa-info.alexa-handler.ts
+++ apps/app-webservice/src/app/domain/alexa-handlers/fisa-info.alexa-handler.ts
@@ -1,14 +1,19 @@
 import { HandlerInput, RequestHandler } from 'ask-sdk-core';
 import { Response } from 'ask-sdk-model';
 
+const text = {
+  intentName: "FisaIntent", // french ?
+  speechText: 'Voici les informations à propos de la fisa.',
+}
+
 export class FisaInfoAlexaHandler implements RequestHandler {
   canHandle(handlerInput: HandlerInput): boolean {
     const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' && request.intent.name === 'AskWeatherIntent';
+    return request.type === 'IntentRequest' && request.intent.name === text.intentName ;
   }
 
   handle(handlerInput: HandlerInput): Response {
-    const speechText = 'Voici les informations à propos de la fisa.';
+    const speechText = text.speechText;
 
     return handlerInput.responseBuilder
       .speak(speechText)
