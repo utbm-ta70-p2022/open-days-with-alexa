ea096e0c51e55d829055d03b9a728ffcb82d7c74
Anthony DUPORT <anthonyduport.ent@gmail.com>
2022-06-17 10:59:47 +0200

    update default image time

--- libs/lib-common/src/lib/models/information.model.ts
+++ libs/lib-common/src/lib/models/information.model.ts
@@ -1,6 +1,6 @@
 export class InformationModel {
   id: string;
-  displayDurationInSeconds: number = 30;
+  displayDurationInSeconds: number = 60;
 
   public constructor(attributes?: Partial<InformationModel>) {
     Object.assign(this, attributes);
